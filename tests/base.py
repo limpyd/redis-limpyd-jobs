36eafc1b4c47e8a5adf4061c56e158e3c32b8e66
twidi <s.angel@twidi.com>
2013-09-07 03:25:08 +0200

    Use unittest2 to work on python 2.6

--- tests/base.py
+++ tests/base.py
@@ -1,46 +1,12 @@
-import unittest
 import sys
-from StringIO import StringIO
-try:
+if sys.version_info >= (2, 7):
     from unittest.case import _AssertRaisesContext
-except ImportError:
-    # _AssertRaisesContext appears in python 2.7 but we want to support 2.6
-    import re
+    import unittest
+else:
+    from unittest2.case import _AssertRaisesContext
+    import unittest2 as unittest
 
-    class _AssertRaisesContext(object):
-        """A context manager used to implement TestCase.assertRaises* methods."""
-
-        def __init__(self, expected, test_case, expected_regexp=None):
-            self.expected = expected
-            self.failureException = test_case.failureException
-            self.expected_regexp = expected_regexp
-
-        def __enter__(self):
-            return self
-
-        def __exit__(self, exc_type, exc_value, tb):
-            if exc_type is None:
-                try:
-                    exc_name = self.expected.__name__
-                except AttributeError:
-                    exc_name = str(self.expected)
-                    raise self.failureException(
-                        "{0} not raised".format(exc_name))
-            if not issubclass(exc_type, self.expected):
-                # let unexpected exceptions pass through
-                return False
-            self.exception = exc_value  # store for later retrieval
-            if self.expected_regexp is None:
-                return True
-
-            expected_regexp = self.expected_regexp
-            if isinstance(expected_regexp, basestring):
-                expected_regexp = re.compile(expected_regexp)
-            if not expected_regexp.search(str(exc_value)):
-                raise self.failureException('"%s" does not match "%s"' %
-                        (expected_regexp.pattern, str(exc_value)))
-            return True
-
+from StringIO import StringIO
 
 from limpyd.database import DEFAULT_CONNECTION_SETTINGS
 from limpyd.contrib.database import PipelineDatabase
