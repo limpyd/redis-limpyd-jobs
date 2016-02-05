70e4ca25326e0c40d38933176849f4a4b7983c83
Stephane Angel (Twidi) <s.angel@twidi.com>
2016-02-05 16:16:44 +0100

    Bump to version 0.1.4

--- limpyd_jobs/version.py
+++ limpyd_jobs/version.py
@@ -2,7 +2,7 @@
 
 __all__ = ('VERSION', '__author__', '__contact__', '__homepage__', '__version__')
 
-VERSION = (0, 1, 3)
+VERSION = (0, 1, 4)
 
 __author__ = 'Stephane "Twidi" Angel'
 __contact__ = "s.angel@twidi.com"
